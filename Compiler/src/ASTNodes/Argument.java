e5bd5765adafceb5e9b1bf1c19f597c7d5d7ddb2
Sadegh Zareiyan <mohammadzare947@yahoo.com>
2019-07-12 18:01:58 +0430

    AST classes designed

--- Compiler/src/ASTNodes/Argument.java
+++ Compiler/src/ASTNodes/Argument.java
@@ -8,7 +8,6 @@ public class Argument implements CodeGeneratable {
 
     public Type type;
     public String identifier;
-    public ArrayList<Argument> arguments;
 
     public Argument() {
     }
@@ -16,12 +15,8 @@ public class Argument implements CodeGeneratable {
     public Argument(Type type, String identifier) {
         this.type = type;
         this.identifier = identifier;
-        this.arguments = new ArrayList<>();
     }
 
-    public void addElement(Argument arg){
-        this.arguments.add(arg);
-    }
 
     @Override
     public String generateCode() {
